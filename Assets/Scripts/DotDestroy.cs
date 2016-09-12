e79ca866cf8e318de306c2fbe11f0ced3d1729a8
RainbowRabbit <grucha112@gmail.com>
2016-09-12 11:32:51 +0200

    Spawn
    Spawn randomowy w ustalonych wcześniej miejscach, nie mam warunków
    kolizji więc narazie jest timer do destroya

---
+++ Assets/Scripts/DotDestroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotDestroy : MonoBehaviour {
+
+	public float destroyTime = 5.0f;  //zastosowałem timer, nie mam warunków destroya
+
+	// Use this for initialization
+	void Start () 
+	{
+		Destroy (gameObject, destroyTime);
+	}
+
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Transform[] SpawnPoints;
+	public float SpawnTime = 0.5f;
+
+	public GameObject Dots;
+	// pozniej zrobimy public GameObject[] Dots;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating ("Spawn", SpawnTime, 5); // 5 trzeba zamienic z ilością kolizji, najlepiej jakaś tablica
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	void Spawn()
+	{
+		int spawnIndex = Random.Range (0, SpawnPoints.Length); //w zaleznosci ile spawnpointow, od razu robi random
+		Instantiate (Dots, SpawnPoints[spawnIndex].position, SpawnPoints[spawnIndex].rotation);
+
+	}
+}
